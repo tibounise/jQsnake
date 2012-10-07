8d9546ece8ffcbdd192d3f525c34f3b21675d5a2
Jean Thomas <tibounise@gmail.com>
2012-10-07 18:46:00 +0200

    Remove unnecessary comments

--- js/keyboard.js
+++ js/keyboard.js
@@ -27,86 +27,86 @@ function initAzerty() {
 				// HUMAN PLAYER 1 KEYS //
 				case 122:
 					if (direction_hp3 != DOWN) {
-						direction_hp1 = UP; // Up
+						direction_hp1 = UP;
 					}
 					break;
 				case 115:
 					if (direction_hp3 != UP){
-						direction_hp1 = DOWN; // Down
+						direction_hp1 = DOWN;
 					}
 					break;
 				case 113:
 					if (direction_hp3 != RIGHT){
-						direction_hp1 = LEFT; // Left
+						direction_hp1 = LEFT;
 					}
 					break;
 				case 100:
 					if (direction_hp3 != LEFT){
-						direction_hp1 = RIGHT; // Right
+						direction_hp1 = RIGHT;
 					}
 					break;
 
 				// HUMAN PLAYER 2 KEYS //
 				case 102:
 					if (direction_hp3 != DOWN) {
-						direction_hp2 = UP; // Up
+						direction_hp2 = UP;
 					}
 					break;
 				case 103:
 					if (direction_hp3 != UP){
-						direction_hp2 = DOWN; // Down
+						direction_hp2 = DOWN;
 					}
 					break;
 				case 104:
 					if (direction_hp3 != RIGHT){
-						direction_hp2 = LEFT; // Left
+						direction_hp2 = LEFT;
 					}
 					break;
 				case 116:
 					if (direction_hp3 != LEFT){
-						direction_hp2 = RIGHT; // Right
+						direction_hp2 = RIGHT;
 					}
 				
 				// HUMAN PLAYER 3 KEYS //
 				case 105:
 					if (direction_hp3 != DOWN){
-						direction_hp3 = UP; // Up
+						direction_hp3 = UP;
 					}
 					break;
 				case 107:
 					if (direction_hp3 != UP) {
-						direction_hp3 = DOWN; // Down
+						direction_hp3 = DOWN;
 					}
 					break;
 				case 106:
 					if (direction_hp3 != RIGHT){
-						direction_hp3 = LEFT; // Left
+						direction_hp3 = LEFT;
 					}
 					break;
 				case 108:
 					if (direction_hp3 != LEFT){
-						direction_hp3 = RIGHT; // Right
+						direction_hp3 = RIGHT;
 					}
 					
 				// HUMAN PLAYER 4 KEYS //
 				case 56:
 					if (direction_hp3 != DOWN) {
-						direction_hp4 = UP; // Up
+						direction_hp4 = UP;
 					}
 					break;
 				case 53:
 					if (direction_hp3 != UP){
-						direction_hp4 = DOWN; // Down
+						direction_hp4 = DOWN;
 					}
 					break;
 				case 52:
 					if (direction_hp3 != RIGHT){
-						direction_hp4 = LEFT; // Left
+						direction_hp4 = LEFT;
 					}
 					break;
 				case 54:
 					if (direction_hp3 != LEFT){
-						direction_hp4 = RIGHT; // Right
+						direction_hp4 = RIGHT;
 					}
 
 				default:
@@ -138,86 +138,86 @@ function initQwerty() {
 				// HUMAN PLAYER 1 KEYS //
 				case 119:
 					if (direction_hp3 != DOWN) {
-						direction_hp1 = UP; // Up
+						direction_hp1 = UP;
 					}
 					break;
 				case 115:
 					if (direction_hp3 != UP){
-						direction_hp1 = DOWN; // Down
+						direction_hp1 = DOWN;
 					}
 					break;
 				case 97:
 					if (direction_hp3 != RIGHT){
-						direction_hp1 = LEFT; // Left
+						direction_hp1 = LEFT;
 					}
 					break;
 				case 100:
 					if (direction_hp3 != LEFT){
-						direction_hp1 = RIGHT; // Right
+						direction_hp1 = RIGHT;
 					}
 					break;
 
 				// HUMAN PLAYER 2 KEYS //
 				case 102:
 					if (direction_hp3 != DOWN) {
-						direction_hp2 = UP; // Up
+						direction_hp2 = UP;
 					}
 					break;
 				case 103:
 					if (direction_hp3 != UP){
-						direction_hp2 = DOWN; // Down
+						direction_hp2 = DOWN;
 					}
 					break;
 				case 104:
 					if (direction_hp3 != RIGHT){
-						direction_hp2 = LEFT; // Left
+						direction_hp2 = LEFT;
 					}
 					break;
 				case 116:
 					if (direction_hp3 != LEFT){
-						direction_hp2 = RIGHT; // Right
+						direction_hp2 = RIGHT;
 					}
 				
 				// HUMAN PLAYER 3 KEYS //
 				case 105:
 					if (direction_hp3 != DOWN){
-						direction_hp3 = UP; // Up
+						direction_hp3 = UP;
 					}
 					break;
 				case 107:
 					if (direction_hp3 != UP) {
-						direction_hp3 = DOWN; // Down
+						direction_hp3 = DOWN;
 					}
 					break;
 				case 106:
 					if (direction_hp3 != RIGHT){
-						direction_hp3 = LEFT; // Left
+						direction_hp3 = LEFT;
 					}
 					break;
 				case 108:
 					if (direction_hp3 != LEFT){
-						direction_hp3 = RIGHT; // Right
+						direction_hp3 = RIGHT;
 					}
 					
 				// HUMAN PLAYER 4 KEYS //
 				case 56:
 					if (direction_hp3 != DOWN) {
-						direction_hp4 = UP; // Up
+						direction_hp4 = UP;
 					}
 					break;
 				case 53:
 					if (direction_hp3 != UP){
-						direction_hp4 = DOWN; // Down
+						direction_hp4 = DOWN;
 					}
 					break;
 				case 52:
 					if (direction_hp3 != RIGHT){
-						direction_hp4 = LEFT; // Left
+						direction_hp4 = LEFT;
 					}
 					break;
 				case 54:
 					if (direction_hp3 != LEFT){
-						direction_hp4 = RIGHT; // Right
+						direction_hp4 = RIGHT;
 					}
 
 				default:
